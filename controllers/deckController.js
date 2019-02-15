be77cda0fec196d51cdcf1ee35338285dd712386
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-02-15 01:03:44 -0600

    edit deck info works

--- controllers/deckController.js
+++ controllers/deckController.js
@@ -52,5 +52,57 @@ router.post("/", async (req, res)=>{
 	}
 })
 
+// update deck: 
+router.patch("/edit/:id", async (req, res) => {
+
+	try {
+
+		const update = {
+			ownerId: req.body.userId,
+			name: req.body.name,
+			description_short: req.body.description_short,
+			description_long: req.body.description_long
+		}
+
+		console.log("UPDATE: ", update);
+
+		// update deck in DB: 
+		const updatedDeck = await Deck.findByIdAndUpdate(req.params.id, update, {new: true} );
+
+		console.log("UPDATED DECK: ", updatedDeck);
+
+		// update on User: 
+
+		const foundUser = await User.findById(req.body.userId);
+
+		console.log("FOUND USER: ", foundUser);
+
+		const targetIndex = foundUser.decks.findIndex((deck)=>{
+			if (deck._id.toString() === req.params.id) {
+				return true
+			} else {
+				return false 
+			}
+		})
+
+		foundUser.decks.splice(targetIndex, 1, updatedDeck);
+
+		const updatedUser = await foundUser.save();
+
+		console.log("UPDATED USER: ", updatedUser);
+
+		res.json({
+			status: 200,
+			data: {action: "Updated Deck, user with Deck"}
+		})
+	} catch (err) {
+		console.log(err);
+		res.send(err)
+	}
+})
+
+
 //  ========== EXPORT ROUTER  ==========
 module.exports = router;
+
+
